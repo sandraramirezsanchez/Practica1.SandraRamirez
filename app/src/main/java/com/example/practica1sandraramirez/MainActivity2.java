1f5984cf737c3bfca4b37cdda0a84939f44ee338
sandraramirezsanchez <61703821+sandraramirezsanchez@users.noreply.github.com>
2021-03-08 16:54:15 -0500

    Hola se realizo cambios en el archivo

---
+++ app/src/main/java/com/example/practica1sandraramirez/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.practica1sandraramirez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView nameUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle valores = getIntent().getExtras();
+        String user = valores.getString("usuario");
+
+        nameUser = findViewById(R.id.tvuser);
+        nameUser.setText(user);
+
+    }
+}
